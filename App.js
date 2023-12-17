e12c89a5a4909d12b90320ad6e275fabeaa9e3eb
Pranay Kothari <pranay.kothari109@gmail.com>
2023-12-17 14:54:32 +0530

    add tailwind & fetch api data

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { ActivityIndicator, FlatList, SafeAreaView, Text, View } from 'react-native';
+
+const Country = ({ country }) => {
+  console.log('country', country.item.name?.common)
+  const countryDetail = country.item;
+
+  const name = countryDetail.name?.common;
+  
+  return (
+    <View>
+      <Text>{name}</Text>
+      {/* <Text>{name}</Text>
+      <Text>{capital}</Text>
+      <Text>{region}</Text> */}
+    </View>
+  )
+}
 
 export default function App() {
+
+  const [isLoading, setLoading] = useState(true);
+  const [countries, setCountries] = useState([]);
+
+  const getCountries = async () => {
+    try {
+      const uri = 'https://restcountries.com/v3.1/all';
+      const res = await axios.get(uri);
+      setCountries(res.data);
+    } catch (err) {
+      console.log(err)
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    getCountries();
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      {isLoading ? (
+        <ActivityIndicator />
+      ) :
+        (<SafeAreaView className="">
+          <FlatList
+            data={countries}
+            renderItem={(country) => (
+              <Country
+                country={country}
+              // keyExtractor={item => item.id}
+              // extraData={selectedId}
+              />)}
+          />
+        </SafeAreaView>)
+      }
     </View>
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
